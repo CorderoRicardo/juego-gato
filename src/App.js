ad3237b49c23aeaff2ee0241eaf517a338a81cce
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-04 21:28:08 -0600

    (WIP) Refactor Moves component

--- src/App.js
+++ src/App.js
@@ -81,22 +81,10 @@ function Board({ xIsNext, squares, onPlay }) {
     );
 }
 
-export default function Game() {
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [currentMove, setCurrentMove] = useState(0);
-    const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove];
-
-    function handlePlay(nextSquares) {
-        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-        setHistory(nextHistory);
-        setCurrentMove(nextHistory.length - 1);
-    }
-
+function Moves({ history, setCurrentMove, currentMove }) {
     function jumpTo(nextMove) {
         setCurrentMove(nextMove);
     }
-
     const moves = history.map((squares, move) => {
         let description;
         if (move > 0) {
@@ -110,6 +98,38 @@ export default function Game() {
             </li>
         );
     });
+    return <ol>{moves}</ol>;
+}
+
+export default function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    // function jumpTo(nextMove) {
+    //     setCurrentMove(nextMove);
+    // }
+
+    // const moves = history.map((squares, move) => {
+    //     let description;
+    //     if (move > 0) {
+    //         description = `Go to move #${move}`;
+    //     } else {
+    //         description = 'Go to game start';
+    //     }
+    //     return (
+    //         <li key={move}>
+    //             <button onClick={() => jumpTo(move)}>{description}</button>
+    //         </li>
+    //     );
+    // });
     return (
         <div className='game'>
             <div className='game-board'>
@@ -120,7 +140,12 @@ export default function Game() {
                 />
             </div>
             <div className='game-info'>
-                <ol>{moves}</ol>
+                {/* <ol>{moves}</ol> */}
+                <Moves
+                    history={history}
+                    setCurrentMove={setCurrentMove}
+                    currentMove={currentMove}
+                />
             </div>
         </div>
     );
